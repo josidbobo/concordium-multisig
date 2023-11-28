b3745cf7d675ddec32415193712f450d6692fb01
Joshua Okoro <josidbobo@gmail.com>
2023-11-28 14:08:47 +0100

    chore: transfer logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,20 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 //! # A Concordium V1 smart contract
+use concordium_smart_contract_testing::Transfer;
 use concordium_std::{collections::*, *};
 use core::fmt::Debug;
 
 #[derive(Serialize, SchemaType, Clone)]
 struct InitialiseParams{
-    timeout : Timestamp,
-
+    timeout : Duration,
     #[concordium(size_length = 2)]
     signers: BTreeSet<AccountAddress>,
 
     min_signers_req: u16
 }
 /// Your smart contract state.
-#[derive(Serial, SchemaType, DeserialWithState)]
+#[derive(Serial, DeserialWithState)]
 #[concordium(state_parameter = "St")]
 pub struct State<St>{
     // Arguments initialising the smart contract
@@ -34,6 +34,7 @@ pub struct Request{
 }
 
 type RequestId = u64;
+type CustomisedResult<T> = Result<T, ErrorOnReceive>;
 
 #[derive(Serialize, SchemaType)]
 pub enum RequestAction {
@@ -54,17 +55,33 @@ pub enum Error {
     AccountsLessThanSupportNeeded
 }
 
+#[derive(Debug, PartialEq, Eq, Reject, Serialize, SchemaType)]
 pub enum ErrorOnReceive{
-    NotReceived
+    #[from(ParseError)]
+    ParseParams,
+    // Only accounts allowed to send to this contract are permitted
+    NotRegisteredAccount,
+    // Time elapsed
+    TimedOut,
+    // Receiving account not eligible or invalid
+    InvalidRecipient,
+    // Same Request to transfer can't be made twice ie using same request id
+    AlreadyExists,
+    // Requested amount more than balance
+    InsufficientBalance,
+    // Only normal accounts can send to this contract
+    NotUserAccount,
+    //
+    Overflow,
 }
 
 /// Init function that creates a new smart contract.
 #[init(contract = "multi_sig", parameter = "InitialiseParams", payable)]
-fn init<St: HasStateApi>(_ctx: &InitContext, amount: Amount, _state_builder: &mut StateBuilder<St>) -> Result<State<St>, Error> {
+fn init<St: HasStateApi>(_ctx: &InitContext, _state_builder: &mut StateBuilder<St>, amount: Amount,) -> Result<State<St>, Error> {
     // Your code
     let parameter: InitialiseParams = match _ctx.parameter_cursor().get(){
         Ok(parameter) => parameter,
-        Err(_) => return Err(Reject::default()),
+        Err(_) => Err(Error::ParseParams).unwrap(),
     };
     ensure!(parameter.min_signers_req <= parameter.signers.len() as u16, Error::AccountsLessThanSupportNeeded);
 
@@ -78,23 +95,100 @@ fn init<St: HasStateApi>(_ctx: &InitContext, amount: Amount, _state_builder: &mu
     Ok(state)
 }
 
+impl From<TransferError> for ErrorOnReceive {
+    fn from(re: TransferError) -> Self{
+        match re{
+            TransferError::AmountTooLarge => Self::InsufficientBalance,
+            TransferError::MissingAccount => Self::InvalidRecipient,
+        }
+    }
+}
+
 /// Receive function. The input parameter is the boolean variable `throw_error`.
 ///  If `throw_error == true`, the receive function will throw a custom error.
 ///  If `throw_error == false`, the receive function executes successfully.
 #[receive(
     contract = "multi_sig",
-    name = "receive",
+    name = "deposit",
     error = "Error",
     payable
 )]
-#[inline(always)]
-fn receive<St: HasStateApi>(ctx: &ReceiveContext, amount: Amount, _host: &mut Host<State<St>>) -> Result<(), Error> {
+//#[inline(always)]
+fn deposit<St: HasStateApi>(ctx: &ReceiveContext, _host: &impl HasHost<State<St>, StateApiType = St>, amount: Amount,) -> Result<(), Error> {
     // Your code
         Ok(())
 }
 
-/// View function that returns the content of the state.
-#[receive(contract = "multi_sig", name = "view", return_value = "State")]
-fn view<'b>(_ctx: &ReceiveContext, host: &'b Host<State>) -> ReceiveResult<&'b State> {
-    Ok(host.state())
+/// Receive function that returns the status of the state.
+#[receive(contract = "multi_sig", mutable, name = "receive", payable, error = "ErrorOnReceive", parameter = "RequestAction")]
+fn message<St: HasStateApi>(_ctx: &ReceiveContext, host: &impl HasHost<State<St>, StateApiType = St>, amount: Amount) -> CustomisedResult<()> {
+let sender = _ctx.sender();
+
+// Get info on the interating address if its a contract or user Account
+let address_type = match sender{
+    Address::Contract(_) => Err(ErrorOnReceive::NotUserAccount),
+    Address::Account(address) => Ok(address),
+};
+let valid_address = address_type.unwrap();
+
+// Validate that the account is part of those authorised to call this contract
+ensure!(host.state().init_params.signers.iter().any(|signers| sender.matches_account(signers)), ErrorOnReceive::NotRegisteredAccount);
+
+let now = _ctx.metadata().slot_time();
+let ozi = _ctx.parameter_cursor().get().unwrap();
+match ozi {
+    RequestAction::SeekTransfer(transfer_amount, account_to_receive, id) => {
+        let mut balance_reserved = Amount::zero();
+        let mut requests_active: BTreeMap<RequestId, Request> = BTreeMap::new();
+        for(y, q) in host.state().requests.iter() {
+            if q.expiry > now{
+                requests_active.insert(*y, q.clone());
+                balance_reserved += q.amount;
+            }
+        }
+        // Keep the request still alive or not yet expired
+        host.state_mut().requests = host.state_builder().new_map();
+        for (b, e) in requests_active.iter(){
+            host.state_mut().requests.insert(*b, e.clone());
+        }
+
+        let mut has = false;
+        for (p, _) in host.state().requests.iter(){
+            if *p == id{
+                has = true;
+                break;
+            }
+        }
+        ensure!(!has, ErrorOnReceive::AlreadyExists);
+
+        let balance = amount + host.self_balance();
+        ensure!(
+            balance - balance_reserved >= transfer_amount,
+            ErrorOnReceive::InsufficientBalance
+        );
+
+        let mut in_favour = BTreeSet::new();
+        in_favour.insert(valid_address);
+
+        let new_request = Request{
+            amount: transfer_amount,
+            sender_account: valid_address,
+            accounts_in_aggrement: in_favour,
+            receiver: account_to_receive,
+            expiry: now.checked_add(host.state().init_params.timeout).ok_or(ErrorOnReceive::Overflow)?
+        };
+
+        host.state_mut().requests.insert(id, new_request);
+        Ok(())
+    }
+
+    RequestAction::AcceptTransfer(amut, recipient_account, idd) => {
+        let transfer = {
+            let 
+        }
+    }
+
+
+}
+
 }
